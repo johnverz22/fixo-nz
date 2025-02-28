50e36bd7b1652dbb498870552cd04e6f4a7b78e0
RicardoMacagba <155034677+RicardoMacagba@users.noreply.github.com>
2025-03-01 00:35:33 +0800

    Initial commit to new repo

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|confirmed|string|min:6'
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password)
+        ]);
+        $user->save();
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+        
+            'user' => $user,
+            'access_token' => $token,
+            // 'token_type' => 'Bearer',
+        ]);
+    }
+}
